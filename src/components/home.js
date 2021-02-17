4cac1821e4444871ef25f33c69378657f26a4361
Mitchell Bean <mitchtroybean@gmail.com>
2021-02-16 23:07:28 -0800

    search functionality working for name and email

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import List from "./list.js";
+import Searchlist from "./search";
 
 export default class home extends Component {
   state = {
+    name: "",
     results: [],
+    Filteredperson: [],
+    search: "",
   };
 
   componentDidMount() {
@@ -14,19 +18,58 @@ export default class home extends Component {
   searchAPI = () => {
     API.search()
       .then((res) => {
-        this.setState({ results: res.data.results });
+        this.setState({
+          results: res.data.results,
+          Filteredperson: res.data.results,
+        });
+
         console.log(res);
       })
-
       .catch((err) => console.log(err));
+  };
+
+  handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  // filterResults = () => {
+  //   this.setState({
+  //     Filteredperson: results.filter((dog) => {
+  //       console.log(dog, "!!!!!!!!!!!");
+  //       dog.name.last.includes(this.state.search);
+  //     }),
+  //   });
+  // };
+
+  // When the form is submitted, search the Giphy API for `this.state.search`
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      Filteredperson: this.state.results.filter((dog) => {
+        console.log(dog.name.last, this.state.search, "!!!!!!!!!!!");
+        return (
+          dog.name.last.includes(this.state.search) ||
+          dog.name.first.includes(this.state.search) ||
+          dog.email.includes(this.state.search)
+        );
+      }),
+    });
   };
 
   render() {
     return (
       <div>
-        HELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLO
+        <Searchlist
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
         <br></br>
-        <List results={this.state.results} />
+        <List results={this.state.Filteredperson} />
       </div>
     );
   }
--- src/components/list.js
+++ src/components/list.js
@@ -1,18 +1,25 @@
 import React from "react";
+import "./list.css";
 
 function List(props) {
   return (
-    <ul className="list">
-      {props.results.map((results) => (
-        <li className="list" key={results.cell}>
-          <img
-            alt={results.title}
-            className="img"
-            src={results.picture.medium}
-          />
-        </li>
-      ))}
-    </ul>
+    <div className="table">
+      <ul className="list">
+        {props.results.map((results) => (
+          <li className="listtwo" key={results.cell}>
+            <img
+              alt={results.title}
+              className="img"
+              src={results.picture.large}
+            />
+            <br></br>
+            <span>
+              {results.name.first} {results.name.last}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
